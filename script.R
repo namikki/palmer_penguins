f6944c46a75c9bf101851c4945cb6660d04ff2e6
Bianca da Silva Rodrigues <85198342+namikki@users.noreply.github.com>
2023-02-04 20:08:29 -0300

    adicionando um arquivo README.Rmd ao projeto
    Foi adicionado um arquvio README.Rmd ao projeto e feita alterações no script. As alterações foram na parte de visualização onde adicionei nomes nos eixo X e Y no gráfico e um título.

--- script.R
+++ script.R
@@ -1,10 +1,3 @@
-# Bibliotecas e Pacotes
-install.packages(tidyverse)
-install.packages(palmerpenguins)
-library(tidyverse)
-library(palmerpenguins)
-library(ggplot2)
-
 # Visualizando nosso dataframe (essa função mostra as 6 primeiras linhas)
 head(penguins)
 
@@ -20,7 +13,10 @@ penguins %>%
 
 #Primeira visualização de dados (usando estética de cores e forma)
 ggplot(data = penguins) +
-  geom_point(mapping = aes(x = flipper_length_mm, y = body_mass_g, color=species, shape=species))
+  geom_point(mapping = aes(x = flipper_length_mm, y = body_mass_g, color=species, shape=species)) +
+  xlab("Comprimento das Nadadeiras (mm)") +
+  ylab("Massa Corporal (g)") +
+  ggtitle("Relação entre Massa Corporal e Comprimento das Nadadeiras")
 
 
 
