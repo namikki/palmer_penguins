c541629dc0f2611cebfbfd4bc9be8043eb3cd2f3
Bianca da Silva Rodrigues <85198342+namikki@users.noreply.github.com>
2023-02-06 17:47:30 -0300

    nova visualização com a função facet_wrap()

--- script.R
+++ script.R
@@ -1,19 +1,18 @@
-# Visualizando nosso dataframe (essa função mostra as 6 primeiras linhas)
-head(penguins)
 
-#Contando a quantidade de linhas que o dataframe possui
-nrow(penguins)
-
-#Contando quantos valores únicos as colunas species e island possuem
-penguins %>% 
-  count(species)
-
-penguins %>% 
-  count(island)
-
-#Primeira visualização de dados (usando estética de cores e forma)
+#Analisando a relação entre o comprimento das nadadeiras e a massa corporal 
+#das 3 espécies de pinguins (Gráfico de Dispersão)
 ggplot(data = penguins) +
   geom_point(mapping = aes(x = flipper_length_mm, y = body_mass_g, color=species, shape=species)) +
+  xlab("Comprimento das Nadadeiras (mm)") +
+  ylab("Massa Corporal (g)") +
+  ggtitle("Relação entre Massa Corporal e Comprimento das Nadadeiras")
+
+#Analisando a relação entre o comprimento das nadadeiras e a massa corporal 
+#das 3 espécies de pinguins (Gráfico de Dispersão) usando a função facet_wrap
+#para criar um gráfico separado para cada variável(espécie)
+ggplot(data=penguins)+
+  geom_point(mapping = aes(x=flipper_length_mm,y=body_mass_g,color=species))+
+  facet_wrap(~species)+
   xlab("Comprimento das Nadadeiras (mm)") +
   ylab("Massa Corporal (g)") +
   ggtitle("Relação entre Massa Corporal e Comprimento das Nadadeiras")
